c6157c994222efd4c96bebcc47c647e97e85dc3e
Ahmed jamal <95928456+Ag994@users.noreply.github.com>
2023-01-01 05:22:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
-import matplotlib.pyplot as plt
 import matplotlib as pylab
 import seaborn as sns
 import numpy as np
